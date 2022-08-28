628e59fe9f6738dbe8b904b5069a492077e47105
Berkan Marasli <berkan.marasli@hotmail.com>
2022-08-28 21:14:33 +0100

    Added margin between experiences under my journey

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,7 @@
 import "./Header.css"
 import logo from "../../images/BM-logo.png"
-// import linkedin from "../../icons/linkedin.png"
-// import github from "../../icons/github.png"
-// import email from "../../icons/email.png"
-import { ICON } from "../../icons"
-import { EXTERNAL_LINKS } from "../../constants"
+// import { ICON } from "../../icons"
+// import { EXTERNAL_LINKS } from "../../constants"
 
 function Header(props) {
   const { displayMainPage } = props.functions
@@ -36,19 +33,20 @@ function AppHeaderInnerRight() {
 }
 
 function displayIcons() {
-  return (
-    <div>
-      <a href={EXTERNAL_LINKS.linkedinProfile}>
-        <img src={ICON.linkedin} alt="LinkedIn Icon" className="App-Header-inner-right-icon"></img>
-      </a>
-      <a href={EXTERNAL_LINKS.githubProfile}>
-        <img src={ICON.github} alt="Github Icon" className="App-Header-inner-right-icon"></img>
-      </a>
-      <a href={`mailto:${EXTERNAL_LINKS.emailAddress}`}>
-        <img src={ICON.email} alt="Email Icon" className="App-Header-inner-right-icon"></img>
-      </a>
-    </div>
-  )
+  // return (
+  //   <div>
+  //     <a href={EXTERNAL_LINKS.linkedinProfile}>
+  //       <img src={ICON.linkedin} alt="LinkedIn Icon" className="App-Header-inner-right-icon"></img>
+  //     </a>
+  //     <a href={EXTERNAL_LINKS.githubProfile}>
+  //       <img src={ICON.github} alt="Github Icon" className="App-Header-inner-right-icon"></img>
+  //     </a>
+  //     <a href={`mailto:${EXTERNAL_LINKS.emailAddress}`}>
+  //       <img src={ICON.email} alt="Email Icon" className="App-Header-inner-right-icon"></img>
+  //     </a>
+  //   </div>
+  // )
+  return <p>THIS NEEDS SOME CONTENT</p>
 }
 
 export default Header
